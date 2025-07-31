284412ff3458ac79fc92d2c9a12c0f3d6cb551e1
si <psiluv@gmail.com>
2025-07-31 12:46:00 +0900

    [feature]29

--- prime_factor.py
+++ prime_factor.py
@@ -3,4 +3,8 @@ class PrimeFactor:
 
     def of(self, param)-> []:
         factors = []
+        if param == 1:
+            return factors
+        if param == 2:
+            factors.append(2)
         return factors
